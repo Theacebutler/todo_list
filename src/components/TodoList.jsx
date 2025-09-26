11c0961b5038ca34b1364f09539f81796d029a1d
Theacebutler <Theacebutler@gmail.com>
2025-09-26 00:17:59 -0400

    added no todo case

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,18 +6,27 @@ import '../css/TodoList.css'
 export default function TodoList(props) {
 
     const { todos } = props
-
+if (todos.length > 0) {
     return (
+        <>
         <ul className='main'>
             {todos.map((task, taskIndex) => {
                 return (
                     <>
                     <TodoCard {...props} key={taskIndex} index={taskIndex}>
-                        <p>{task.charAt(0).toUpperCase() + task.slice(1)} </p>
+                        <p>{task.charAt(0).toUpperCase() + task.slice(1)}</p>
                     </TodoCard>
                     </>
                 )
             })}
         </ul>
+        </>
+    )}
+else {
+    return (
+        <li className='no-todo'>
+            <h3 className=''>No todo's yet!</h3>
+        </li>
     )
+}
 }
